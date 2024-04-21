395f47cf8e6ad0ef5cbd9d1f1184cf61743b2e7d
Gagansai9 <gagansai.gudi@gmail.com>
2024-04-21 15:24:20 +0530

    Games have been added

--- script.js
+++ script.js
@@ -10,17 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
     let autoplayInterval;
 
     const autoPlay = () => { 
-        // Calculate the total width of all cards 
         const totalCardWidth = carousel.scrollWidth;
         
-        // Calculate the maximum scroll position 
         const maxScrollLeft = totalCardWidth - carousel.offsetWidth;
         
-        // If the carousel is at the end, scroll to the beginning
         if (carousel.scrollLeft >= maxScrollLeft) {
             carousel.scrollLeft = 0;
         } else {
-            // Autoplay the carousel to scroll by one card width
             carousel.scrollLeft += firstCardWidth;
         }
     }; 
@@ -42,16 +38,14 @@ document.addEventListener("DOMContentLoaded", function() {
         timeoutId = setTimeout(startAutoPlay, 1000);
     }); 
 
-    // Add event listeners for the arrow buttons to 
-    // scroll the carousel left and right 
-    arrowBtns.forEach(btn => { 
-        btn.addEventListener("click", () => { 
-            carousel.scrollLeft += btn.id === "left" ? 
+    
+    arrowBtns.forEach(bt => { 
+        bt.addEventListener("click", () => { 
+            carousel.scrollLeft += bt.id === "left" ? 
                 -firstCardWidth : firstCardWidth; 
         }); 
     }); 
     
-    // Start autoplaying the carousel initially
     startAutoPlay();
 });
 
